123cdab9913504f3c23fa2b9988f7ab4578be3e9
YariArdiansyah <2006017@itg.ac.id>
2023-03-09 09:14:00 +0700

    Tugas Sec 5 - Basic Linux Command (Eksplorasi)

---
+++ 5_Basic_Linux_Command_(Eksplorasi)/Praktikum/Yari.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Halo teman teman semuanya !"
+echo "Silakan masukkan nama Anda : "
+read nama
+echo "Halo, $nama!"
+echo "Tanggal hari ini adalah : "
+date
+echo "Berikut ini adalah daftar file di direktori ini: "
+ls
